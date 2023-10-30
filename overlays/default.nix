8d27e0bb968a2572b63a43123a64c6b3712e0028
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-10-30 22:07:36 +0100

    tdlib & telega: bump

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,7 +14,7 @@ inputs: {
     # });
 
     tdlib = prev.tdlib.overrideAttrs (oldAttrs: {
-      version = "1.8.19";
+      version = "1.8.20";
       src = final.fetchFromGitHub {
         owner = "tdlib";
         repo = "td";
@@ -22,8 +22,10 @@ inputs: {
         # The tdlib authors do not set tags for minor versions, but
         # external programs depending on tdlib constrain the minor
         # version, hence we set a specific commit with a known version.
-        rev = "2589c3fd46925f5d57e4ec79233cd1bd0f5d0c09";
-        hash = "sha256-mbhxuJjrV3nC8Ja7N0WWF9ByHovJLmoLLuuzoU4khjU=";
+        # rev = "2589c3fd46925f5d57e4ec79233cd1bd0f5d0c09";
+        # hash = "sha256-mbhxuJjrV3nC8Ja7N0WWF9ByHovJLmoLLuuzoU4khjU=";
+				rev = "dd77e4628f1a65f332f0cb81c82a19c0fcfa40c2";
+				sha256 = "1j1ckya3dxh2jkxa0xbnpxmbq407cc6z15qf6pn2c496jap2k7h0";
       };
     });
 
--- system/modules/editor/emacs.nix
+++ system/modules/editor/emacs.nix
@@ -30,12 +30,15 @@ in {
           with epkgs; [
             # melpaPackages.telega is outdated. Pull a newer version directly from the repo
             (melpaPackages.telega.overrideAttrs (oldAttrs: {
-              version = "1.8.160";
+              # version = "1.8.160";
+              version = "1.8.200";
               src = pkgs.fetchFromGitHub {
                 owner = "zevlg";
                 repo = "telega.el";
-                rev = "17bfa50c8f2e70daeb8866f3bf195f15623ab520";
-                sha256 = "1057zr4g8llxmzy47l5klyi89x66q8qx5vrd50pmpsp4c6772jz9";
+                # rev = "17bfa50c8f2e70daeb8866f3bf195f15623ab520";
+                # sha256 = "1057zr4g8llxmzy47l5klyi89x66q8qx5vrd50pmpsp4c6772jz9";
+								rev = "93fedcefd6585dd98d649e2d8b8ca393e288f2fd";
+								sha256 = "0d88mc3as1q1lkzjxnbiq8an80nyd9xkz2d9gbcfdfpd3ggm03mz";
               };
             }))
             vterm
