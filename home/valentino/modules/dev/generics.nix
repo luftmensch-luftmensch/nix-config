1e8fc1033dcd4f894fff73c15401b1069c8088bf
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-02-18 21:58:29 +0100

    Bug fixes

--- home/valentino/modules/dev/generics.nix
+++ home/valentino/modules/dev/generics.nix
@@ -4,40 +4,42 @@
   pkgs,
   ...
 }:
-with lib; let
+with lib;
+with builtins; let
   cfg = config.valentino.modules.dev.generics;
-  # FIXME: find out the error
-  # apply-hm-env = pkgs.writeShellScript "apply-hm-env" ''
-  #   ${lib.optionalString (config.home.sessionPath != []) ''
-  #     export PATH=${builtins.concatStringsSep ":" config.home.sessionPath}:$PATH
-  #   ''}
-  #   ${builtins.concatStringsSep "\n" (lib.mapAttrsToList (k: v: ''
-  #       export ${k}=${v}
-  #     '')
-  #     config.home.sessionVariables)}
-  #   ${config.home.sessionVariablesExtra}
-  #   exec "$@"
-  # '';
-  # # runs processes as systemd transient services
-  # run-as-service = pkgs.writeShellScriptBin "run-as-service" ''
-  #   exec ${pkgs.systemd}/bin/systemd-run \
-  #     --slice=app-manual.slice \
-  #     --property=ExitType=cgroup \
-  #     --user \
-  #     --wait \
-  #     bash -lc "exec ${apply-hm-env} $@"
-  # '';
+
+  apply-hm-env = pkgs.writeShellScript "apply-hm-env" ''
+    ${optionalString (config.home.sessionPath != []) ''
+      export PATH=${concatStringsSep ":" config.home.sessionPath}:$PATH
+    ''}
+    ${concatStringsSep "\n" (lib.mapAttrsToList (k: v: ''
+        export ${toString k}=${toString v}
+      '')
+      config.home.sessionVariables)}
+    ${config.home.sessionVariablesExtra}
+    exec "$@"
+  '';
+  # runs processes as systemd transient services
+  run-as-service = pkgs.writeShellScriptBin "run-as-service" ''
+    exec ${pkgs.systemd}/bin/systemd-run \
+      --slice=app-manual.slice \
+      --property=ExitType=cgroup \
+      --user \
+      --wait \
+      bash -lc "exec ${apply-hm-env} $@"
+  '';
 in {
   options.valentino.modules.dev.generics = {
     enable = mkEnableOption "uncategorized packages for programming";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      httpie-desktop
-      insomnium
-      mongodb-compass
-      # run-as-service
-    ];
+    home.packages =
+      [
+        pkgs.httpie-desktop
+        pkgs.insomnium
+        pkgs.mongodb-compass
+      ]
+      ++ [run-as-service];
   };
 }
