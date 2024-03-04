2f8a64b6418d0e08f4b15a5ee934362504c0f3ec
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-03-04 12:28:50 +0100

    [dev+browser] Minor edits

--- home/valentino/modules/browsers/chromium.nix
+++ home/valentino/modules/browsers/chromium.nix
@@ -5,7 +5,7 @@
 }:
 with lib; let
   cfg = config.valentino.modules.browsers.chromium;
-  inherit (config.valentino.modules) wayland xorg;
+  inherit (config.valentino.modules) wayland;
 in {
   options.valentino.modules.browsers.chromium = {
     enable = mkEnableOption "chromium";
@@ -16,21 +16,8 @@ in {
       enable = true;
       # chrome://flags/#enable-webrtc-pipewire-capturer (Enable it to share entire screen)
       commandLineArgs =
-        [
-          "--force-dark-mode"
-          "--no-first-run"
-
-          "--no-default-browser-check"
-          "--no-service-autorun"
-          "--enable-features=WebUIDarkMode"
-        ]
-        ++ optionals wayland.enable [
-          "--enable-features=UseOzonePlatform"
-          "--ozone-platform=wayland"
-          "--enable-features=WebRTCPipeWireCapturer"
-          "--enable-usermedia-screen-capturing"
-        ]
-        ++ optionals xorg.enable ["--ozone-platform-hint=auto"];
+        ["--force-dark-mode" "--no-first-run" "--no-default-browser-check" "--no-service-autorun" "--enable-features=WebUIDarkMode"]
+        ++ optionals wayland.enable ["--ozone-platform=wayland" "--enable-usermedia-screen-capturing"];
 
       extensions = [
         {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock
--- home/valentino/modules/dev/generics.nix
+++ home/valentino/modules/dev/generics.nix
@@ -28,12 +28,29 @@ with builtins; let
       --wait \
       bash -lc "exec ${apply-hm-env} $@"
   '';
+
+  runbg = pkgs.writeShellScriptBin "runbg" ''
+    [ $# -eq 0 ] && {
+        echo "$(basename "$0"): missing command" >&2
+        exit 1
+    }
+    prog="$(which "$1")"
+    [ -z "$prog" ] && {
+        echo "$(basename "$0"): unknown command: $1" >&2
+        exit 1
+    }
+    shift  # remove $1, now $prog, from args
+    tty -s && exec </dev/null      # if stdin is a terminal, redirect from null
+    tty -s <&1 && exec >/dev/null  # if stdout is a terminal, redirect to null
+    tty -s <&2 && exec 2>&1        # stderr to stdout (which might not be null)
+    "$prog" "$@" &
+  '';
 in {
   options.valentino.modules.dev.generics = {
     enable = mkEnableOption "uncategorized packages for programming";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [run-as-service];
+    home.packages = [run-as-service runbg];
   };
 }
