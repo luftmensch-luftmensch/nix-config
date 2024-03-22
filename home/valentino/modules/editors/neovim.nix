fe9ba0e5132b35ef74077ea78444aa2cf27b5ab1
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-03-22 13:14:43 +0100

    Updates:
    * flake: bump;
    * Moved neovim dependencies to neovim-flake

--- home/valentino/modules/editors/neovim.nix
+++ home/valentino/modules/editors/neovim.nix
@@ -8,13 +8,9 @@
 with lib; let
   cfg = config.valentino.modules.editors.neovim;
 in {
-  options.valentino.modules.editors.neovim = {
-    enable = mkEnableOption "neovim";
-  };
+  options.valentino.modules.editors.neovim.enable = mkEnableOption "neovim";
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs;
-      [cppcheck nodePackages.bash-language-server]
-      ++ (with inputs.neovim-flake.packages.${pkgs.system}; [nvim]);
+    home.packages = with inputs.neovim-flake.packages.${pkgs.system}; [nvim];
   };
 }
