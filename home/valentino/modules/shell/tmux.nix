0e822d1dccb29779e221ff36dae27907a89a3050
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-03-28 11:54:50 +0100

    [tmux] Minor edits

--- home/valentino/modules/shell/tmux.nix
+++ home/valentino/modules/shell/tmux.nix
@@ -1,14 +1,28 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib; let
   cfg = config.valentino.modules.shell.tmux;
+  theme = with config.colorScheme.palette; ''
+    thm_bg=\"#${strings.toLower base00}\"
+    thm_fg=\"#${strings.toLower base05}\"
+    thm_cyan=\"#${strings.toLower base0C}\"
+    thm_black=\"#${strings.toLower base01}\"
+    thm_gray=\"#${strings.toLower base01}\"
+    thm_magenta=\"#${strings.toLower base0E}\"
+    thm_pink=\"#${strings.toLower base0E}\"
+    thm_red=\"#${strings.toLower base08}\"
+    thm_green=\"#${strings.toLower base0B}\"
+    thm_yellow=\"#${strings.toLower base09}\"
+    thm_blue=\"#${strings.toLower base0D}\"
+    thm_orange=\"#${strings.toLower base0A}\"
+    thm_black4=\"#${strings.toLower base00}\"
+  '';
 in {
-  options.valentino.modules.shell.tmux = {
-    enable = mkEnableOption "tmux configuration";
-  };
+  options.valentino.modules.shell.tmux.enable = mkEnableOption "tmux configuration";
 
   config = mkIf cfg.enable {
     programs.tmux = {
@@ -19,11 +33,13 @@ in {
       clock24 = true;
       historyLimit = 8000;
       escapeTime = 20;
-      terminal = "\${TERM}";
+      # terminal = "\${TERM}";
+      terminal = "tmux-256color";
       sensibleOnTop = false;
       mouse = true;
 
       extraConfig = ''
+        set -ga terminal-overrides ",alacritty*:Tc"
         set -g pane-base-index 1 # renumber any window when any is closed
         setw -g automatic-rename on   # rename window to reflect current program
         set -g set-titles on          # set terminal title
@@ -47,6 +63,55 @@ in {
         # maximize current pane
         bind + run "cut -c3- '#{TMUX_CONF}' | sh -s _maximize_pane '#{session_name}' '#D'"
       '';
+
+      plugins = with pkgs; [
+        {
+          plugin = tmuxPlugins.catppuccin.overrideAttrs (_: {
+            version = "unstable-2024-03-24";
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "tmux";
+              rev = "e2d345648cb7e56302ee82fec6cafbbd8db23129";
+              hash = "sha256-WCJfDjYUNWqkDyCgYKADDS0SeEpfMnZ57bCsp9steh8=";
+            };
+            postInstall = ''
+              echo "${theme}" > $target/catppuccin-dynamic.tmuxtheme
+            '';
+          });
+          extraConfig = with config.colorScheme.palette; ''
+            set -g @catppuccin_flavour "dynamic"
+
+            set -g @catppuccin_status_background "default"
+
+            set -g @catppuccin_status_left_separator "█"
+            set -g @catppuccin_status_right_separator "█"
+            set -g @catppuccin_window_middle_separator " █"
+            set -g @catppuccin_window_number_position "right"
+
+            set -g @catppuccin_window_default_fill "number"
+            set -g @catppuccin_window_default_text "#W"
+
+            set -g @catppuccin_window_current_fill "number"
+            set -g @catppuccin_window_current_text "#W"
+            set -g @catppuccin_window_current_background "#${strings.toLower base02}"
+
+            set -g @catppuccin_status_modules_right "directory session"
+
+            set -g @catppuccin_weather_icon " "
+            set -g @catppuccin_uptime_icon "󰔟 "
+            set -g @catppuccin_load_icon "󰊚 "
+            set -g @catppuccin_battery_icon "#{battery_icon}"
+            set -g @catppuccin_host_icon "󰒋 "
+            set -g @catppuccin_cpu_icon " "
+            set -g @catppuccin_application_icon " "
+            set -g @catppuccin_clima_icon " "
+            set -g @catppuccin_directory_icon " "
+            set -g @catppuccin_date_time_icon "󰃰 "
+            set -g @catppuccin_user_icon " "
+            set -g @catppuccin_session_icon " "
+          '';
+        }
+      ];
     };
   };
 }
