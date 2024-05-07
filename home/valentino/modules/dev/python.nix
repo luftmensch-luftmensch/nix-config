a63b90bf0b8afffe380331918a98cfe87508ad96
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-05-07 12:39:46 +0200

    [python] customized configuration

--- home/valentino/modules/dev/python.nix
+++ home/valentino/modules/dev/python.nix
@@ -6,17 +6,12 @@
 }:
 with lib; let
   cfg = config.valentino.modules.dev.python;
+
+  pyEnv = pkgs.python3.withPackages (ps: with ps; [flake8 python-lsp-server httpie pygments]);
 in {
   options.valentino.modules.dev.python.enable = mkEnableOption "python language support and language server";
 
   config = mkIf cfg.enable {
-    home.packages =
-      [pkgs.python311]
-      ++ (with pkgs.python311Packages; [
-        flake8
-        python-lsp-server
-        httpie
-        pygments
-      ]);
+    home.packages = [pyEnv];
   };
 }
