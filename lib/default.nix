a8fc75f6eec9ce4ed6021513a9098d329b3c5296
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-12-18 11:17:21 +0100

    lib: customization

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,15 @@
-{inputs, ...}: let
+{ inputs, ... }:
+let
   inherit (inputs.nixpkgs) lib;
-in rec {
-  supportedSystems = ["x86_64-linux" "aarch64-linux"];
+in
+rec {
+  supportedSystems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
   forAllSystems = lib.genAttrs supportedSystems;
 
-  mkSystem = import ./system.nix {inherit inputs;};
-  mkHome = import ./home.nix {inherit inputs;};
+  mkSystem = import ./system.nix { inherit inputs; };
+  mkHome = import ./home.nix { inherit inputs; };
+  whenT = k: t: if k then t else [ ];
 }
