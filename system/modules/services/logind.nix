4d5a30f9724eb89bde6f6c7122abcfbabfff2fcb
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-01-02 13:17:30 +0100

    system: poweroff -> suspend on lidSwitch

--- system/modules/services/logind.nix
+++ system/modules/services/logind.nix
@@ -3,15 +3,17 @@
   lib,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.system.modules.services.logind;
-in {
+in
+{
   options.system.modules.services.logind.enable = mkEnableOption "Enable logind capabilities";
 
   config = mkIf cfg.enable {
     services.logind = {
       lidSwitch = "suspend";
-      lidSwitchDocked = "poweroff";
+      lidSwitchDocked = "suspend";
     };
   };
 }
