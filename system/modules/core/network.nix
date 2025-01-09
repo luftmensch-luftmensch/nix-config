24ccf1feb245a3d44eef7723b78334a6a3aaf8eb
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-01-09 21:13:19 +0100

    networking: updates

--- system/modules/core/network.nix
+++ system/modules/core/network.nix
@@ -7,6 +7,7 @@
     # specific directories
     useDHCP = false;
     useNetworkd = lib.mkForce true;
+    enableIPv6 = false;
     firewall =
       let
         localRanges = [
@@ -23,19 +24,18 @@
         allowedUDPPorts = [ 51820 ];
       };
 
-    nameservers = [
-      # DNS recursive service w/ Quad9 on ipv4 & ipv6
-      "9.9.9.9"
-      "149.112.112.112"
-
-      "2620:fe::fe"
-      "2620:fe::9"
-    ];
-
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
       wifi.powersave = true;
+      insertNameservers = [
+        # DNS recursive service w/ Quad9 on ipv4 & ipv6
+        "9.9.9.9"
+        "149.112.112.112"
+
+        "2620:fe::fe"
+        "2620:fe::9"
+      ];
     };
   };
 
