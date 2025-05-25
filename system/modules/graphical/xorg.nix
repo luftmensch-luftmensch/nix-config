1fa9aa03b5508459835f1d52d6453c9361ec854c
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-05-25 21:20:29 +0200

    system: fixed pam support for i3lock

--- system/modules/graphical/xorg.nix
+++ system/modules/graphical/xorg.nix
@@ -4,7 +4,8 @@ let
   cfg = config.system.modules.graphical.xorg;
 in
 {
-  options.system.modules.graphical.xorg.enable = mkEnableOption "xorg basic configuration and packages";
+  options.system.modules.graphical.xorg.enable =
+    mkEnableOption "xorg basic configuration and packages";
 
   config = mkIf cfg.enable {
     services.xserver = {
@@ -14,5 +15,7 @@ in
     };
 
     services.displayManager.defaultSession = "none+i3";
+    # https://github.com/NixOS/nixpkgs/issues/401891
+    security.pam.services.i3lock.enable = true;
   };
 }
